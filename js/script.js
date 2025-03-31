bbe6207200516541f121454f4e475868d90efa62
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-31 15:56:20 +0200

    [UPD] Creata funzione che ritorna una stringa da aggiungere successivamente alla sezione cards

--- js/script.js
+++ js/script.js
@@ -36,3 +36,35 @@ const teamMembers = [
       img: "img/female3.png"
     }
   ];
+
+// Functions
+// Funzione per creare la stringa da aggiungere successivamente
+const createCard = (obj) => { 
+    const card = `<div id="cards" class="d-flex">
+                    <div class="card d-flex">
+                        <div class="card-image">
+                            <img src="./img/${obj.image}" alt="" class="w-100" id="image">
+                        </div>
+                        <div class="card-content d-flex align-items-center">
+                            <ul>
+                                <li>
+                                    <h3 id="name">${obj.name}</h3>
+                                </li>
+                                <li id="role">
+                                    ${obj.role}
+                                </li>
+                                <li id="email">
+                                    <address><a href="#">${obj.email}</a></address>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>`
+
+    return card
+
+}
+
+for (i = 0; i < teamMembers.length; i++){
+    createCard(teamMembers[i])
+}
